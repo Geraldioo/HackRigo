59d88b66c4eb783db0faad09dd5f32234f047b94
Geraldioo <bryangeraldioo@gmail.com>
2024-04-02 17:45:52 +0700

    layouting card

--- hack-rigo/src/app/page.tsx
+++ hack-rigo/src/app/page.tsx
@@ -1,15 +1,91 @@
-
-import { Carousel } from "@/components/carousel";
-import Navbar from "@/components/navbar";
+import { Carousel } from "@/components/Carousel";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div className=" dark:bg-white w-full">
       <Navbar />
       <div className="flex justify-center w-screen h-screen dark:bg-white">
         <Carousel />
       </div>
-    </>
+      <div className="grid justify-center dark:bg-white grid-cols-3 mx-40 border-t border-black">
+        <div className="card bg-whit shadow-xl mr-4">
+          <figure className="pt-10 ">
+            <img
+              src="https://erigostore.co.id/cdn/shop/files/id-11134201-7r98t-lsckgevvnkep29_14b9b91e-f4a4-40e3-95b3-40e873f7c6f0.jpg?v=1709293881"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <p className="mb-5">Erigo Koko Short Sleeve Bordir Zachary White</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-whit shadow-xl mr-4">
+          <figure className="pt-10 ">
+            <img
+              src="https://erigostore.co.id/cdn/shop/files/id-11134201-7r98t-lsckgevvnkep29_14b9b91e-f4a4-40e3-95b3-40e873f7c6f0.jpg?v=1709293881"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <p className="mb-5">Erigo Koko Short Sleeve Bordir Zachary White</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-whit shadow-xl mr-4">
+          <figure className="pt-10 ">
+            <img
+              src="https://erigostore.co.id/cdn/shop/files/id-11134201-7r98t-lsckgevvnkep29_14b9b91e-f4a4-40e3-95b3-40e873f7c6f0.jpg?v=1709293881"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <p className="mb-5">Erigo Koko Short Sleeve Bordir Zachary White</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-whit shadow-xl mr-4">
+          <figure className="pt-10 ">
+            <img
+              src="https://erigostore.co.id/cdn/shop/files/id-11134201-7r98t-lsckgevvnkep29_14b9b91e-f4a4-40e3-95b3-40e873f7c6f0.jpg?v=1709293881"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <p className="mb-5">Erigo Koko Short Sleeve Bordir Zachary White</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-whit shadow-xl mr-4">
+          <figure className="pt-10 ">
+            <img
+              src="https://erigostore.co.id/cdn/shop/files/id-11134201-7r98t-lsckgevvnkep29_14b9b91e-f4a4-40e3-95b3-40e873f7c6f0.jpg?v=1709293881"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <p className="mb-5">Erigo Koko Short Sleeve Bordir Zachary White</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- hack-rigo/src/components/Navbar.tsx
+++ hack-rigo/src/components/Navbar.tsx
@@ -3,10 +3,12 @@ export default function Navbar() {
     <>
       <div className="navbar bg-[#A4B0DC] sticky top-0 z-50">
         <div className="flex-1">
-          <img
-            src="https://erigostore.co.id/cdn/shop/files/Erigo_White_Logo_50de1a9b-2216-4056-a575-0820d287508b.png?v=1691658501&width=300"
-            className="btn btn-ghost w-[20%] h-[10%]"
-          />
+          <button>
+            <img
+              src="https://erigostore.co.id/cdn/shop/files/Erigo_White_Logo_50de1a9b-2216-4056-a575-0820d287508b.png?v=1691658501&width=300"
+              className="h-5 ml-6"
+            />
+          </button>
         </div>
         <div className="flex-none gap-2 mr-5">
           <div className="form-control ">
